41e8c9cf0da66028139ba476a6f6381a37061eaf
smartconstructiongroup <119409598+smartconstructiongroup@users.noreply.github.com>
2023-01-24 03:21:35 -0600

    Preprocessing- video to frames

---
+++ Converting_Video_to_Frames.py
@@ -0,0 +1,15 @@
+
+import cv2
+ 
+# Opens the Video file
+cap= cv2.VideoCapture('G:/Dr.Maghrebi/1.mp4')
+i=0
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    cv2.imwrite('img'+str(i)+'.jpg',frame)
+    i+=1
+ 
+cap.release()
+cv2.destroyAllWindows()
